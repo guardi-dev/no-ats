c1f48e72f23f04cb7b82bea874972865072217d0
root <godofluck89@gmail.com>
2026-04-18 14:35:47 +0000

    remove unused classes

--- app/jobs/[id]/page.tsx
+++ app/jobs/[id]/page.tsx
@@ -59,7 +59,7 @@ export default async function JobDetailPage(props: { params: Promise<{ id: strin
                 <article 
                     className={`
                         max-w-2xl mx-auto prose prose-neutral
-                        [&_ol]:ml-5 [&_ul]:ml-5 []
+                        [&_ol]:ml-5 [&_ul]:ml-5
                     `}>
                     <Markdown
                         remarkPlugins={[remarkGfm]}>
