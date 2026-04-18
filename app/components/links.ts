7c15b3a90c6024fd3fa2e0929a66b0d6cba79956
root <godofluck89@gmail.com>
2026-04-18 11:55:55 +0000

    fix links

--- app/components/links.ts
+++ app/components/links.ts
@@ -1,10 +1,11 @@
+const source = "https://github.com/guardi-dev/no-ats/"
 
 export const links = {
     home: "/",
     external: {
-        sourceCode: "https://github.com/guardi/no-ats/",
-        manifesto: "https://github.com/guardi/no-ats/MANIFESTO.md",
-        rules: "https://github.com/guardi/no-ats/RULES.md",
-        blacklist: "https://github.com/guardi/no-ats/BLACKLIST.md"
+        sourceCode: source,
+        manifesto: source + "blob/main/MANIFESTO.md",
+        rules: source + "blob/main/RULES.md",
+        blacklist: source + "blob/main/BLACKLIST.md"
     }
 }
